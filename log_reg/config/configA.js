82de028028010ec8c0b4aae6cae956f61a81ed95
yunPu <admin@example.com>
2018-05-23 16:03:41 +0800

    211

--- log_reg/config/configA.js
+++ log_reg/config/configA.js
@@ -6,10 +6,7 @@ connection.select=function(sql,param){
 	 var promise = new Promise(function(resolve,reject) {//resolve成功,reject失败
         var result = null;
         var connection = mysql.createConnection({
-		    host:'qdm114284563.my3w.com',
-            user:'qdm114284563',
-            password:'whfstt1985',
-            database:'qdm114284563_db',  //鏁版嵁搴撳悕绉[13;12H port:'3306'
+		
 		});
 		connection.connect();
         connection.query(sql, param,function (err, results, fields) { 
